3b63ede7f336262866372878888f35d7762b48d2
melchongx <melfredfonclara@gmail.com>
2023-05-21 21:31:30 +0800

    sd

--- frontend/home.php
+++ frontend/home.php
@@ -12,10 +12,10 @@
 		<h2><a href="home.html">Tiger Journal</a></h2>
 		<nav>
 			<ul>
-				<a href="home.html"><li>Home</li></a>
-				<a href="stories.html" onmouseover="hoverOnStories()" ><li>Stories</li></a>
-				<a href="aboutus.html"><li>About Us</li></a>
-				<a href="login.html"><li>Login</li></a>
+				<a href="home.php"><li>Home</li></a>
+				<a href="stories.php" onmouseover="hoverOnStories()" ><li>Stories</li></a>
+				<a href="aboutus.php"><li>About Us</li></a>
+				<a href="login.php"><li>Login</li></a>
 			</ul>
 		</nav>
 		<hr>
@@ -23,9 +23,9 @@
 	<hr class="headerLine" id="headerHrID">
 	<div id="storiesSubULID" class="storiesSubUL" onmouseover="hoverOnStories()" onmouseout="hoverOffStories()"> <!-- sublist for stories -->
 		<ul>
-			<a class="subUL" href="news.html"><li>News</li></a>
-			<a class="subUL" href="sports.html"><li>Sports</li></a>
-			<a class="subUL" href="memos.html"><li>Memos</li></a>
+			<a class="subUL" href="news.php"><li>News</li></a>
+			<a class="subUL" href="sports.php"><li>Sports</li></a>
+			<a class="subUL" href="memos.php"><li>Memos</li></a>
 		</ul>
 	</div>
 	<div class="homeContent">
