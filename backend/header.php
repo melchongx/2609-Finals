5c3c073d6f6082fd3d524629bb4fbc0042b50ffc
melchongx <melfredfonclara@gmail.com>
2023-05-23 00:37:35 +0800

    adjust

---
+++ backend/header.php
@@ -0,0 +1,20 @@
+    <header onmouseover="hoverOnHeader()" onmouseout="hoverOffHeader()">
+		<h2><a href="home.php">Tiger Journal</a></h2>
+		<nav>
+			<ul>
+				<a href="home.php"><li>Home</li></a>
+				<a href="stories.php" onmouseover="hoverOnStories()" ><li>Stories</li></a>
+				<a href="aboutus.php"><li>About Us</li></a>
+				<a href="login.php"><li>Login</li></a>
+			</ul>
+		</nav>
+		<hr>
+	</header>
+	<hr class="headerLine" id="headerHrID">
+	<div id="storiesSubULID" class="storiesSubUL" onmouseover="hoverOnStories()" onmouseout="hoverOffStories()"> <!-- sublist for stories -->
+		<ul>
+			<a class="subUL" href="news.php"><li>News</li></a>
+			<a class="subUL" href="sports.php"><li>Sports</li></a>
+			<a class="subUL" href="memos.php"><li>Memos</li></a>
+		</ul>
+	</div>
--- frontend/login.php
+++ frontend/login.php
@@ -8,37 +8,21 @@
 	<script src="script.js"></script>
 </head>
 <body>
-	<header onmouseover="hoverOnHeader()" onmouseout="hoverOffHeader()">
-		<h2><a href="home.php">Tiger Journal</a></h2>
-		<nav>
-			<ul>
-				<a href="home.php"><li>Home</li></a>
-				<a href="stories.php" onmouseover="hoverOnStories()" ><li>Stories</li></a>
-				<a href="aboutus.php"><li>About Us</li></a>
-				<a href="login.php"><li>Login</li></a>
-			</ul>
-		</nav>
-		<hr>
-	</header>
-	<hr class="headerLine" id="headerHrID">
-	<div id="storiesSubULID" class="storiesSubUL" onmouseover="hoverOnStories()" onmouseout="hoverOffStories()"> <!-- sublist for stories -->
-		<ul>
-			<a class="subUL" href="news.php"><li>News</li></a>
-			<a class="subUL" href="sports.php"><li>Sports</li></a>
-			<a class="subUL" href="memos.php"><li>Memos</li></a>
-		</ul>
-	</div>
+	<?php include_once '..//backend//header.php';  ?>
 	<div class="loginContent">
         <div class="loginBox">
-            <form>
+            <form class="login">
 				<label for="userName">Username</label>
-				<input type="text" id="userName" name="userName">
-				<label for="userName">Username</label>
-				<input type="text" id="userName" name="userName">
-				<label for="userName">Username</label>
-				<input type="text" id="userName" name="userName">
-				<label for="userName">Username</label>
-				<input type="text" id="userName" name="userName">
+				<input type="text" id="userName" name="userName"></br>
+				<label for="passWord">Password</label>
+				<input type="password" id="passWord" name="passWord">
+				<hr>
+				<span>
+					<center>
+					<button type="submit">Login</button> or 
+					<a href="home.php"><button type="button">Sign Up</button></a>
+					</center>
+				</span>
 			</form>
         </div>
     </div>
